5b55b012b5476dd1741a541272c9b3cd6570ff0e
Rajesh604 <kanugurajesh3@gmail.com>
2023-09-21 21:24:05 +0530

    add

--- src/CheckoutForm.tsx
+++ src/CheckoutForm.tsx
@@ -60,7 +60,7 @@ export default function CheckoutForm() {
       elements,
       confirmParams: {
         // Make sure to change this to your payment completion page
-        return_url: "http://localhost:3000",
+        return_url: "http://localhost:4242",
       },
     });
 
